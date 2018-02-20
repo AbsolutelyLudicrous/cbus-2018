58a7b6a434f2c7f604f5d6dd61cca35de660d70e
AbsolutelyLudicrous <eostre.danne@protonmail.com>
2018-02-19 22:17:56 -0500

    Turns out scanning directories is harder than that

--- data/index.php
+++ data/index.php
@@ -15,7 +15,25 @@
 		<h3>Index:</h3>
 		<p>
 			<?php
-				echo(scandir("/usr/local/www/apache24"));
+				// Create recursive dir iterator which skips dot folders
+				$dir = new RecursiveDirectoryIterator('.',
+					FilesystemIterator::SKIP_DOTS);
+				
+				// Flatten the recursive iterator, folders come before their files
+				$it  = new RecursiveIteratorIterator($dir,
+					RecursiveIteratorIterator::SELF_FIRST);
+				
+				// Maximum depth is 1 level deeper than the base folder
+				$it->setMaxDepth(1);
+				
+				// Basic loop displaying different messages based on file or folder
+				foreach ($it as $fileinfo) {
+					if ($fileinfo->isDir()) {
+						printf("Folder - %s\n", $fileinfo->getFilename());
+					} elseif ($fileinfo->isFile()) {
+						printf("File From %s - %s\n", $it->getSubPath(), $fileinfo->getFilename());
+					}
+				}
 			?>
 		</p>
 	</body>

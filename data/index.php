0fa367a670ad132e0bc12545ced8cf9fc16da378
Danne <mdanne@protonmail.com>
2018-02-19 22:31:13 -0500

    Removed that chunk of garbage code, reworked list

--- data/index.php
+++ data/index.php
@@ -15,25 +15,9 @@
 		<h3>Index:</h3>
 		<p>
 			<?php
-				print_r(scandir("."));
-
-				// Create recursive dir iterator which skips dot folders
-				$dir = new RecursiveDirectoryIterator('.', FilesystemIterator::SKIP_DOTS);
-				
-				// Flatten the recursive iterator, folders come before their files
-				$it  = new RecursiveIteratorIterator($dir, RecursiveIteratorIterator::SELF_FIRST);
-				
-				// Maximum depth is 1 level deeper than the base folder
-				$it->setMaxDepth(1);
-				
-				// Basic loop displaying different messages based on file or folder
-				foreach ($it as $fileinfo) {
-					if ($fileinfo->isDir()) {
-						printf("dir: - %s\n", $fileinfo->getFilename());
-					} elseif ($fileinfo->isFile()) {
-						printf("file: %s - %s\n", $it->getSubPath(), $fileinfo->getFilename());
-					}
-				}
+				echo '<pre>'
+				print_r(scandir('.'));
+				echo '</pre>'
 			?>
 		</p>
 	</body>

2e123f5f6f94b78d8f147932af5e6f28c18c0866
AbsolutelyLudicrous <eostre.danne@protonmail.com>
2018-02-20 00:48:17 -0500

    no fucking way this works

--- data/index.php
+++ data/index.php
@@ -19,7 +19,12 @@
 				// echo '<print_r(scandir('.'));
 				// echo '<echo '</pre>';
 				foreach (scandir('.') as $file){
-					echo($file);
+					echo(
+						'<a href="http://cutie-computie.org/',getcwd(),'/'$file,'">',
+						$file,
+						'</a>',
+						'<br>'
+					);
 				}
 			?>
 		</p>

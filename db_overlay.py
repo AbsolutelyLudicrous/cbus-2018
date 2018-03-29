8993f0674c068ab3e68be841bbbc81b7e253e781
AbsolutelyLudicrous <eostre.danne@protonmail.com>
2018-03-29 16:35:22 -0400

    D O C U M E N T A T I O N (tm)

--- db_overlay.py
+++ db_overlay.py
@@ -4,6 +4,12 @@
 	Module which abstracts some basic database functions to the APY
 	Meant to declutter the APY a little bit
 	(Also so that Raghav can do their database wizardry without interference)
+
+	!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	!!                                                                                                                                     !!
+	!!  IT IS ASSUMED THAT ALL REQUESTS TO THIS MODULE ARE PRE-AUTHENTICATED BY APY.PY, THIS MODULE DOES NOT DO ANY REQUEST AUTHETICATION  !!
+	!!                                                                                                                                     !!
+	!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 """
 
 import sqlite3 as sqlite
@@ -23,23 +29,41 @@ if __name__ == "__main__":
 def get_user(u):
 	"""
 	Get all attributes of a specified user
+	Inputs:
+		u - the user to be gotten
 	"""
 	pass
 
 def set_user(u,new_u):
 	"""
 	Set the attributes of a specified user
+	Inputs:
+		u - the user to be replaced
+		new_u - a complete representation of the new user
 	"""
 	pass
 
 def get_user_attr(u,attr):
 	"""
 	Get a single attribute from a user
+	Inputs:
+		u - the user
+		attr - the attribute to be gotten
 	"""
 	pass
 
 def set_user_attr(u,attr):
 	"""
 	Set a single attribute of a user
+	Inputs:
+		u - the user
+		attr - the attribute to be set
 	"""
 	pass
+
+def get_post(p):
+	"""
+	Get the representation of a single post
+	Inputs:
+		p - the ID post to be gotten
+	"""

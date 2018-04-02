7e372e76fc49765238a1d10bd9dfd7d764024048
AbsolutelyLudicrous <eostre.danne@protonmail.com>
2018-04-02 19:57:29 -0400

    Hopefully getting remote connections accepted

--- apy.py
+++ apy.py
@@ -7,13 +7,33 @@
 """
 
 from flask import Flask
+import datetime
+import random
+import socket
 import users_db_overlay as udb
+import posts_db_overlay as pdb
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    return "Hello, World!"
+	return "Hello, World!"
+
+@app.route('/signup', methods=['POST'])
+def mkuser():
+	"""
+	Add a new user to the users database
+	"""
+
+	# generate our 32-character long UUUID
+	recruit_UUUID=str(abs(int(
+		str(hash(datetime.datetime.now))+
+		str(random.randint(111111111111,999999999999))
+	)))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(
+		debug=True, 
+		host=socket.gethostbyname('cutie-computie.org'),
+		port=808
+	)

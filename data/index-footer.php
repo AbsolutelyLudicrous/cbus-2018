8f614344c109fe4eecdb6aaa4ddbf311b57f03d4
AbsolutelyLudicrous <eostre.danne@protonmail.com>
2018-04-15 00:00:26 -0400

    Moving web server directory

--- data/index-footer.php
+++ data/index-footer.php
@@ -2,7 +2,7 @@
 <p>
 	<?php
 		$host="cutie-computie.org";	#hostname of system, can be a hostname or an IP address
-		$leadingdirs="/usr/local/www/apache24/data";	# directories leading up to the web server's hosted data
+		$leadingdirs="/usr/local/www/data";	# directories leading up to the web server's hosted data
 		$rawdir=getcwd();	# raw, unstripped, directory location absolute to the file system
 		$dir=substr($rawdir, (strpos($rawdir,$leadingdirs)+strlen($leadingdirs)));	# stips the raw directory of the server root location
 		foreach (scandir('.') as $file){
--- data/technical.d/info.php
+++ data/technical.d/info.php
@@ -1,5 +1,5 @@
 <?php 
-	if(file_exists('/usr/local/www/apache24/lockfiles/WEBSERVER_DEBUGGING')){
+	if(file_exists('/usr/local/www/lockfiles/WEBSERVER_DEBUGGING')){
 		phpinfo();}
 	else{
 		echo('The webserver does not currently have debugging information turned on, email mdanne@protonmail.com if you would like technical information');}
